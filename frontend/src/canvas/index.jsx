d964285db2de8b50909b13d73639d4f742af3565
AdityaGodara <adityagodara03@gmail.com>
2025-08-27 00:08:16 +0530

    rotation changes

--- frontend/src/canvas/index.jsx
+++ frontend/src/canvas/index.jsx
@@ -10,12 +10,15 @@ const CanvasModel = () => {
             {/* Background Canvas */}
             <Canvas
                 className="absolute top-0 left-0 w-full h-full -z-10"
+                style={{ background: "#1a1a1a" }}
                 camera={{ position: [-2,2,-4], fov: 50 }}
             >
                 <ambientLight intensity={0.5} />
                 <directionalLight position={[5, 5, 5]} intensity={1} />
                 <OrbitControls
                     enableZoom={false}
+                    autoRotate
+                    autoRotateSpeed={0.2}
                 />
                 <NewBox />
             </Canvas>
